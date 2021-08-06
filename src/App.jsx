9bf0e6d08898485be0a143450570696e89e3cacc
Caetano Burjack <caetano.burjack@gmail.com>
2021-08-06 15:21:10 -0300

    starting with React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 export function App() {
     //throw new Error('Testing an Error!')
-    return <h1> Hello ReactJS! </h1>
+    return <RepositoryList />
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,35 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>
+                Repository List
+            </h1>
+            <ul>
+                <li>
+                    <strong>Name</strong>
+                    <p>Short Description</p>
+
+                    <a href="#">Link</a>
+                </li>
+                <li>
+                    <strong>Name</strong>
+                    <p>Short Description</p>
+
+                    <a href="#">Link</a>
+                </li>
+                <li>
+                    <strong>Name</strong>
+                    <p>Short Description</p>
+
+                    <a href="#">Link</a>
+                </li>
+                <li>
+                    <strong>Name</strong>
+                    <p>Short Description</p>
+
+                    <a href="#">Link</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
