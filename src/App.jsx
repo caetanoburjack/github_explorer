967c87bf5576cb3264ad323feca98e6c7cae7d57
Caetano Burjack <caetano.burjack@gmail.com>
2021-08-07 09:48:11 -0300

    starting with react hooks - useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 export function App() {
     //throw new Error('Testing an Error!')
-    return <RepositoryList />
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
